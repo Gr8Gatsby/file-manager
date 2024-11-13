70eede4b7d71e13937878dba191dc063d6fb5c42
Kevin Hill <kevinhill@live.com>
2024-11-13 17:58:49 +0000

    updating build and deploy files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), themePlugin()],
+  base: process.env.GITHUB_PAGES === "true" ? "/file-manager/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
@@ -15,5 +16,30 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const prefix =
+            process.env.GITHUB_PAGES === "true"
+              ? "file-manager/assets"
+              : "assets";
+          return `${prefix}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: (chunkInfo) => {
+          const prefix =
+            process.env.GITHUB_PAGES === "true"
+              ? "file-manager/assets"
+              : "assets";
+          return `${prefix}/[name]-[hash].js`;
+        },
+        entryFileNames: (chunkInfo) => {
+          const prefix =
+            process.env.GITHUB_PAGES === "true"
+              ? "file-manager/assets"
+              : "assets";
+          return `${prefix}/[name]-[hash].js`;
+        },
+      },
+    },
   },
 });
